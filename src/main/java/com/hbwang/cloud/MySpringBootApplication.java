34e02659bc3b9034b0e4e7e591dce97d698779ff
hbwang <136450721@qq.com>
2019-04-08 17:21:13 +0800

    add

--- src/main/java/com/hbwang/cloud/MySpringBootApplication.java
+++ src/main/java/com/hbwang/cloud/MySpringBootApplication.java
@@ -14,6 +14,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement
 public class MySpringBootApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(MySpringBootApplication.class, args);
     }
